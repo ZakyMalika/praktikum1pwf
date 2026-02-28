92d5c4bba5b73a3e4915486bca5ab448bfafc949
ZakyMalika <zakymalika0987@gmail.com>
2026-03-01 00:48:05 +0700

    praktikum kedua

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    {{-- <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('about') }}
+        </h2>
+    </x-slot> --}}
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Nama: Muhammad  Zaky Malika") }} <br>
+                    {{ __("NIM: 20230140017") }} <br>
+                    {{ __("Program Studi: Teknologi Informasi") }} <br>
+                    {{ __("Hobi: Olahraga") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout> 
