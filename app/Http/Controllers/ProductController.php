de55b8fd54005edb02923bdcf55faf9082d7c750
ZakyMalika <zakymalika0987@gmail.com>
2026-04-18 11:02:21 +0700

    praktikum 7- component

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -115,4 +115,5 @@ class ProductController extends Controller
 
         return redirect()->route('product.index')->with('success', 'Product berhasil dihapus');
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,20 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    // Product Page
+    // Rute statis (tanpa parameter {}) taruh di atas
     Route::get('/product', [ProductController::class, 'index'])->name('product.index');
     Route::post('/product', [ProductController::class, 'store'])->name('product.store');
     Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-    Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
-    Route::put('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
-    Route::get('/product/edit/{product}', [ProductController::class, 'edit'])->name('product.edit');
-    Route::delete('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
     Route::get('/product/export', [ProductController::class, 'export'])->name('product.export')->middleware('can:export-product');
-});
 
+    // Rute dengan parameter tambahan spesifik taruh di tengah
+    Route::get('/product/edit/{product}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::put('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
+
+    // Rute dinamis yang menangkap segalanya (/product/{apapun}) WAJIB ditaruh paling bawah
+    Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
